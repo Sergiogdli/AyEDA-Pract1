fa96fa1a58d90d63f27352b1fc2a65dc400659b1
Sergio García <sergiogarcia@macbook-pro-de-sergio.local>
2016-02-15 14:44:54 +0000

    Initial Commit

---
+++ AyEDA-Pract1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AyEDA-Pract1
+//
+//  Created by Sergio García on 15/2/16.
+//  Copyright © 2016 Sergio García. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
